4a0a6612234242efbd57fc5b8ef2065d6d69bf6c
kay的小屋 <47267728+kay-cottage@users.noreply.github.com>
2020-07-30 00:55:55 +0800

    Update Xception_Cell_API.py

--- Xception_Cell_API.py
+++ Xception_Cell_API.py
@@ -26,42 +26,52 @@ app.config['DEBUG'] = True
 
 def main():
 
-    # create model
-    model = load_model(r"\result\model_fine_ep1_valloss3.745.h5")
+    # create model,加载细胞形态学识别模型model_identify_cell.h5文件
+    model = load_model("model_identify_cell.h5")
 
     # load class names
     classes = []
-    with open(r"\result\classes.txt", 'r') as f:
+    with open("classes.txt", 'r') as f:
         classes = list(map(lambda x: x.strip(), f.readlines()))
         print(classes)
 
     # load an input image
-    img = image.load_img(r"\images\2.jpg", target_size=(299, 299))
+    img = image.load_img("2.jpg", target_size=(299, 299))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
    # print(x)
 
-    # predict
+
+
+
+    # 预测打印出识别结果
     pred = model.predict(x)[0]
     global result
     result = [(classes[i], float(pred[i]) * 100.0) for i in range(len(pred))]
     print(result)
+    
+    #返回json格式识别结果
     return json.dumps(result)
     
 
 
 
 
-#设置路由
-@app.route("/",methods = ["GET"])
-
+#设置路由，POST方法请求
+@app.route("/",methods = ["POST"])
 def upload():
+    file_obj = request.files.get("pic")
+    if file_obj is None:
+        return "上传失败"
+    file_obj.save("2.jpg")
+    print("上传成功")
     main()
+    os.remove("2.jpg")
     return json.dumps(result)
 
 
 
 #启动路由
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5000)
